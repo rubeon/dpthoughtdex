a73de42e071ae9575dcdd09f9f3a426c10d4d151
Eric Williams <eric.williams@rackspace.co.uk>
2015-03-11 12:39:54 +0000

    Fixed feeds; added rel=nofollow to user links

--- feeds.py
+++ feeds.py
@@ -1,12 +1,14 @@
 from django.contrib.syndication.views import Feed
-from django.core.urlresolvers import reverse
+from django.core.urlresolvers import reverse_lazy
 from dpthoughtdex.models import Link
 
 class LatestEntriesFeed(Feed):
 
     title = "DPThoughtDex Feed"
-    # link = reverse("link-list-day")
-    link = "http://dev.ehw.io/"
+
+    #link = "http://dev.ehw.io/"
+    # use reverse_lazy to avoid ImproperlyConfigured crap
+    link = reverse_lazy("link-archive")
     
     def items(self):
         return Link.objects.order_by('-pub_date')[:50]
@@ -18,3 +20,4 @@ class LatestEntriesFeed(Feed):
     def item_description(self, item):
         return item.message
     
+
